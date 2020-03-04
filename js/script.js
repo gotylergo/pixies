4bfdc5bfb54ca0bc6828b8db8080b4d5b12a24d8
gotylergo <tk03759@gmail.com>
2020-03-03 22:59:23 -0800

    Track position of mouse/touch

--- js/script.js
+++ js/script.js
@@ -1,11 +1,10 @@
-let x = 1;
-document.onmousemove = function() {
-  x++;
-  document.getElementById('counter').innerHTML = x;
+document.onmousemove = function(e) {
+  document.getElementById('x').innerHTML = e.clientX;
+  document.getElementById('y').innerHTML = e.clientY;
 }
-document.ontouchmove = function() {
-  x++;
-  document.getElementById('counter').innerHTML = x;
+document.ontouchmove = function(e) {
+  document.getElementById('x').innerHTML = e.touches[0].clientX;
+  document.getElementById('y').innerHTML = e.touches[0].clientY;
 }
 document.onmouseleave = function() {
 }
