6aab6eedc4ab03214237a9fa0d7fb5502697d0a8
gotylergo <tk03759@gmail.com>
2020-03-04 00:19:48 -0800

    Smooth transitions. Add instructions per device.

--- js/script.js
+++ js/script.js
@@ -2,16 +2,15 @@ function randomGenerator(position) {
   return `${Math.random() * Math.floor(100) + position}px`;
 }
 
-document.onmousemove = function (e) {
-  document.getElementById('pixie1').style.left = randomGenerator(e.clientX);
-  document.getElementById('pixie1').style.top = randomGenerator(e.clientY);
-  document.getElementById('pixie2').style.left = randomGenerator(e.clientX);
-  document.getElementById('pixie2').style.top = randomGenerator(e.clientY);
-  document.getElementById('pixie3').style.left = randomGenerator(e.clientX);
-  document.getElementById('pixie3').style.top = randomGenerator(e.clientY);
-  document.getElementById('x').innerHTML = e.clientX;
-  document.getElementById('y').innerHTML = e.clientY;
+let isTouchDevice = 'ontouchstart' in document.documentElement;
+
+if (isTouchDevice) {
+  document.getElementById('deviceSpecific').innerHTML = 'Touch the screen';
+} else {
+  document.getElementById('deviceSpecific').innerHTML = 'Move the mouse';
 }
+
+// For touch devices
 document.ontouchmove = function (e) {
   document.getElementById('pixie1').style.left = randomGenerator(e.touches[0].clientX);
   document.getElementById('pixie1').style.top = randomGenerator(e.touches[0].clientY);
@@ -19,12 +18,13 @@ document.ontouchmove = function (e) {
   document.getElementById('pixie2').style.top = randomGenerator(e.touches[0].clientY);
   document.getElementById('pixie3').style.left = randomGenerator(e.touches[0].clientX);
   document.getElementById('pixie3').style.top = randomGenerator(e.touches[0].clientY);
-  document.getElementById('x').innerHTML = e.touches[0].clientX;
-  document.getElementById('y').innerHTML = e.touches[0].clientY;
 }
-document.onmouseleave = function () {
-
-}
-document.ontouchend = function () {
-
+// For mouse devices
+document.onmousemove = function (e) {
+  document.getElementById('pixie1').style.left = randomGenerator(e.clientX);
+  document.getElementById('pixie1').style.top = randomGenerator(e.clientY);
+  document.getElementById('pixie2').style.left = randomGenerator(e.clientX);
+  document.getElementById('pixie2').style.top = randomGenerator(e.clientY);
+  document.getElementById('pixie3').style.left = randomGenerator(e.clientX);
+  document.getElementById('pixie3').style.top = randomGenerator(e.clientY);
 }
