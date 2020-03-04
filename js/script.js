edab731886eaf56ac82ef2797b1dcdaa0c89b52a
gotylergo <tk03759@gmail.com>
2020-03-03 23:37:57 -0800

    Movie Pixie positions following mouse/touch

--- js/script.js
+++ js/script.js
@@ -1,10 +1,30 @@
-document.onmousemove = function(e) {
+function randomGenerator(position) {
+  return `${Math.random() * Math.floor(100) + position}px`;
+}
+
+document.onmousemove = function (e) {
+  document.getElementById('pixie1').style.left = randomGenerator(e.clientX);
+  document.getElementById('pixie1').style.top = randomGenerator(e.clientY);
+  document.getElementById('pixie2').style.left = randomGenerator(e.clientX);
+  document.getElementById('pixie2').style.top = randomGenerator(e.clientY);
+  document.getElementById('pixie3').style.left = randomGenerator(e.clientX);
+  document.getElementById('pixie3').style.top = randomGenerator(e.clientY);
   document.getElementById('x').innerHTML = e.clientX;
   document.getElementById('y').innerHTML = e.clientY;
 }
-document.ontouchmove = function(e) {
+document.ontouchmove = function (e) {
+  document.getElementById('pixie1').style.left = randomGenerator(e.touches[0].clientX);
+  document.getElementById('pixie1').style.top = randomGenerator(e.touches[0].clientY);
+  document.getElementById('pixie2').style.left = randomGenerator(e.touches[0].clientX);
+  document.getElementById('pixie2').style.top = randomGenerator(e.touches[0].clientY);
+  document.getElementById('pixie3').style.left = randomGenerator(e.touches[0].clientX);
+  document.getElementById('pixie3').style.top = randomGenerator(e.touches[0].clientY);
   document.getElementById('x').innerHTML = e.touches[0].clientX;
   document.getElementById('y').innerHTML = e.touches[0].clientY;
 }
-document.onmouseleave = function() {
+document.onmouseleave = function () {
+
+}
+document.ontouchend = function () {
+
 }
